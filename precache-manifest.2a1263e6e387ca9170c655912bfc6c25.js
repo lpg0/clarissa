3e2976b2b519f8a325a5bacb15410e7a66be9746
lpg0 <45275351+lpg0@users.noreply.github.com>
2020-08-28 20:05:31 -0400

    Add files via upload

---
+++ precache-manifest.2a1263e6e387ca9170c655912bfc6c25.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8203058bc6b502ddbf3b0d81a1eeedc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4406dc6952f4c9598b80",
+    "url": "/static/css/main.05e0cbcf.chunk.css"
+  },
+  {
+    "revision": "46a0482a1a58d7c74e2f",
+    "url": "/static/js/2.3ebf5de4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3ebf5de4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4406dc6952f4c9598b80",
+    "url": "/static/js/main.09d1800b.chunk.js"
+  },
+  {
+    "revision": "5ad7524abd7f80c67938",
+    "url": "/static/js/runtime-main.8ee82db6.js"
+  },
+  {
+    "revision": "f8606b94e607137a4dba30247e2093e7",
+    "url": "/static/media/ButtonCircle.f8606b94.png"
+  },
+  {
+    "revision": "09b4cfbba799e9328cf9028da3b47b6e",
+    "url": "/static/media/ButtonMain.09b4cfbb.svg"
+  },
+  {
+    "revision": "18c6b234a43f995c2f2bbd324223162b",
+    "url": "/static/media/button_designs_dark.18c6b234.svg"
+  },
+  {
+    "revision": "a4bb98a1720b78e31f290792f0e53a31",
+    "url": "/static/media/envelope_back.a4bb98a1.svg"
+  },
+  {
+    "revision": "ccbd5793e3160a4ba1f8e8b0d8453324",
+    "url": "/static/media/envelope_closed.ccbd5793.svg"
+  },
+  {
+    "revision": "59515ff6fd46af37b9866cb688a0d911",
+    "url": "/static/media/envelope_open.59515ff6.svg"
+  },
+  {
+    "revision": "86c114be056d3fe8f78e628108e2910c",
+    "url": "/static/media/green_border.86c114be.svg"
+  }
+]);
